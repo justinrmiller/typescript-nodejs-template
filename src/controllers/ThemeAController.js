7c73cde368207d5f4a310f8be4a86758c7e9df13
Justin Miller <justinrmiller@gmail.com>
2023-04-12 17:13:06 -0700

    Improving file hierarchy.

--- src/controllers/ThemeAController.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-class ThemeAController {
-    defaultMethod() {
-        return {
-            text: `You've reached the ${this.constructor.name} default method`
-        };
-    }
-}
-module.exports = new ThemeAController();
